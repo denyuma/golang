6a319331d94fc6e9f79a50558941fb3a91076730
DenYuma <denyuma@DenYumanoMacBook-Pro.local>
2021-07-25 19:00:19 +0900

    atcoder-1

--- main.go
+++ main.go
@@ -1,49 +1,21 @@
 package main
 
-import (
-	"fmt"
-	"os"
-
-	"github.com/urfave/cli"
-)
+import "fmt"
 
 func main() {
-	var (
-		suffix string
-	)
-	app := cli.NewApp()
-	app.Name = "Hello xxxx"
-	app.Usage = "Make `Hello xxx` for arbitrary text"
-	app.Version = "0.1.0"
-
-	app.Flags = []cli.Flag{
-		cli.StringFlag{
-			Name:        "suffix, s",
-			Value:       "!!!",
-			Usage:       "text after speaking something",
-			Destination: &suffix,
-			EnvVar:      "SUFFIX",
-		},
+	var s string
+	t := " chokudai"
+	fmt.Scan(&s)
+	f := make([]int, 9)
+	f[0] = 1
+	for i := 0; i < len(s); i++ {
+		for j := 1; j < 9; j++ {
+			if s[i] == t[j] {
+				f[j] = f[j] + f[j-1]
+				f[j] %= 1000000007
+				fmt.Println(f)
+			}
+		}
 	}
-
-	app.Commands = []cli.Command{
-		{
-			Name:  "hello",
-			Usage: "if use set -t or --text",
-			Flags: []cli.Flag{
-				cli.StringFlag{
-					Name:  "text, t",
-					Value: "world",
-					Usage: "hello xxx text",
-				},
-			},
-
-			Action: func(c *cli.Context) error {
-				fmt.Printf("Hello %s %s\n", c.String("text"), suffix)
-				return nil
-			},
-		},
-	}
-
-	app.Run(os.Args)
+	fmt.Println(f[8] % 1000000007)
 }
