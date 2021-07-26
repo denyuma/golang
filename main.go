a9d88807138ee03e233ffbdc993b8b59f07a3454
DenYuma <denyuma@DenYumanoMacBook-Pro.local>
2021-07-27 01:45:16 +0900

    paiza

--- main.go
+++ main.go
@@ -1,21 +1,72 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+
+type Taxi struct {
+	a     int
+	b     int
+	c     int
+	d     int
+	money int
+}
 
 func main() {
-	var s string
-	t := " chokudai"
-	fmt.Scan(&s)
-	f := make([]int, 9)
-	f[0] = 1
-	for i := 0; i < len(s); i++ {
-		for j := 1; j < 9; j++ {
-			if s[i] == t[j] {
-				f[j] = f[j] + f[j-1]
-				f[j] %= 1000000007
-				fmt.Println(f)
-			}
+	var N, X int
+	var Tarr []Taxi
+	fmt.Scan(&N, &X)
+
+	for i := 0; i < N; i++ {
+		var s []string
+		var narr []int
+		if sc.Scan() {
+			s = strings.Fields(sc.Text())
 		}
+
+		for _, v := range s {
+			a, _ := strconv.Atoi(v)
+			narr = append(narr, a)
+		}
+
+		t := Taxi{
+			a:     narr[0],
+			b:     narr[1],
+			c:     narr[2],
+			d:     narr[3],
+			money: narr[1],
+		}
+
+		Tarr = append(Tarr, t)
 	}
-	fmt.Println(f[8] % 1000000007)
+
+	for i, v := range Tarr {
+		remainLen := X - v.a
+
+		fmt.Println(remainLen)
+		fmt.Println(Tarr[i].money)
+
+		for remainLen >= 0 {
+			Tarr[i].money += v.d
+			fmt.Println(Tarr[i].money)
+			remainLen = remainLen - v.c
+		}
+
+	}
+
+	var moneys []int
+	for _, v := range Tarr {
+		moneys = append(moneys, v.money)
+	}
+
+	sort.Ints(moneys)
+
+	fmt.Println(moneys[0], moneys[len(moneys)-1])
 }
