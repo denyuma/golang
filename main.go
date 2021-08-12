3a44f75f68e77a0f2f6e8139a39580818455c8f3
DenYuma <denyuma@DenYumanoMacBook-Pro.local>
2021-08-12 23:17:22 +0900

    hoge

--- main.go
+++ main.go
@@ -4,69 +4,57 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"sort"
 	"strconv"
 	"strings"
 )
 
 var sc = bufio.NewScanner(os.Stdin)
 
-type Taxi struct {
-	a     int
-	b     int
-	c     int
-	d     int
-	money int
-}
-
 func main() {
-	var N, X int
-	var Tarr []Taxi
-	fmt.Scan(&N, &X)
+	var N, M int
+	var dial [][]string
+	var strs []string
+	if sc.Scan() {
+		arr := strings.Fields(sc.Text())
+		N, _ = strconv.Atoi(arr[0])
+		M, _ = strconv.Atoi(arr[1])
+	}
 
 	for i := 0; i < N; i++ {
-		var s []string
-		var narr []int
 		if sc.Scan() {
-			s = strings.Fields(sc.Text())
+			text := sc.Text()
+			slice := strings.Split(text, "")
+			dial = append(dial, slice)
 		}
-
-		for _, v := range s {
-			a, _ := strconv.Atoi(v)
-			narr = append(narr, a)
-		}
-
-		t := Taxi{
-			a:     narr[0],
-			b:     narr[1],
-			c:     narr[2],
-			d:     narr[3],
-			money: narr[1],
-		}
-
-		Tarr = append(Tarr, t)
 	}
 
-	for i, v := range Tarr {
-		remainLen := X - v.a
-
-		fmt.Println(remainLen)
-		fmt.Println(Tarr[i].money)
-
-		for remainLen >= 0 {
-			Tarr[i].money += v.d
-			fmt.Println(Tarr[i].money)
-			remainLen = remainLen - v.c
+	for i := 0; i < M; i++ {
+		if sc.Scan() {
+			strs = append(strs, sc.Text())
 		}
-
 	}
 
-	var moneys []int
-	for _, v := range Tarr {
-		moneys = append(moneys, v.money)
+	var count = 0
+	var str = ""
+
+	fmt.Println(dial)
+
+	for a := 0; a < N; a++ {
+		for b := 0; b < N; b++ {
+			for i := 0; i < N-b-a+1; i++ {
+				if i+a > 5 && i+b > 5 {
+					break
+				}
+				str += dial[i+a][i+b]
+				fmt.Println(str)
+				for _, v := range strs {
+					if v == str {
+						count++
+					}
+				}
+			}
+			str = ""
+		}
 	}
 
-	sort.Ints(moneys)
-
-	fmt.Println(moneys[0], moneys[len(moneys)-1])
 }
